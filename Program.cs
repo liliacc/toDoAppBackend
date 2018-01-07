e8c7499f86a8a8b114ba1bb1a92f06bd2333726a
Ubuntu <ubuntu@ip-172-31-22-109.eu-west-1.compute.internal>
2018-01-07 18:45:19 +0000

    initialal commit of empty project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace toDoAppBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
