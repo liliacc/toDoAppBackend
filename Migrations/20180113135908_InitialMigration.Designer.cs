e18cf6b5ffc13a840e2d2ac5e7f163333f5d348f
Ubuntu <ubuntu@ip-172-31-20-230.eu-west-1.compute.internal>
2018-01-13 14:15:34 +0000

    adjusting migrations for ms sql server

--- Migrations/20180113135908_InitialMigration.Designer.cs
+++ Migrations/20180113135908_InitialMigration.Designer.cs
@@ -11,9 +11,10 @@ using toDoAppBackend.Entities;
 namespace toDoAppBackend.Migrations
 {
     [DbContext(typeof(ToDoDbContext))]
-    partial class ToDoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180113135908_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
